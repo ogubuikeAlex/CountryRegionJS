c9e0232b5c6f57517f289f6b6b51ee128ac42573
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-04-21 14:57:41 +0100

    feat: add constants to eliminate magic string

---
+++ utilities/constants.js
@@ -0,0 +1,15 @@
+const ZERO = 0;
+const ONE = 1;
+const RESOURCE_URL = 'https://smcore.blob.core.windows.net/countryregion/Countries.json';
+const COUNTRY_WITH_VALID_LOCALS = 160;
+const INVALID_LGA_VALUE = 999;
+const NAME = 'name';
+const ID = 'id';
+
+module.exports = { 
+    RESOURCE_URL, ZERO, 
+    ONE, 
+    COUNTRY_WITH_VALID_LOCALS, 
+    INVALID_LGA_VALUE,
+    NAME, ID
+}
