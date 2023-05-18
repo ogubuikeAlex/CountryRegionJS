36a51456a796c68a8bcabe8ae2e70545bc5e1d10
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-05-18 06:51:35 +0100

    Fix: Update source

--- utilities/constants.js
+++ utilities/constants.js
@@ -1,6 +1,6 @@
 const ZERO = 0;
 const ONE = 1;
-const RESOURCE_URL = 'https://smcore.blob.core.windows.net/countryregion/Countries.json';
+const RESOURCE_URL = 'https://gist.githubusercontent.com/king-Alex-d-great/f13f855ec84d6f30f7f8874a02c115ec/raw/53274df3bf5574963f437ce012bdce7c31ca3305/countries.json';
 const COUNTRY_WITH_VALID_LOCALS = 160;
 const INVALID_LGA_VALUE = 999;
 const NAME = 'name';
